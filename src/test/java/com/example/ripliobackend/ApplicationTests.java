10598de965a275e4fc4d29827cf07ceb9419e8a5
Aleksandar Gadzhinski <alexgadjinski@gmail.com>
2026-08-13 20:51:24 +0300

    chore: initialize main application skeleton

---
+++ src/test/java/com/example/ripliobackend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ripliobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
